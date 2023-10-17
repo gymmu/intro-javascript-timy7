9c4904a83761c871fdcb6c5bb16a03a22f956339
Yeva Skotar <e270611@edu.sbl.ch>
2023-10-17 12:24:00 +0300

    3 Aufgaben

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ function changeBackground() {
   const body = document.querySelector("body")
   body.style.backgroundColor = "darkgrey"
 }
+function deleteText (){
+  const elemToDeleteText = document.querySelector("#to-delete")
+elemToDeleteText.textContent = "Inhalt gelöscht"
+}
+function incremenCounter(){
+  
+}
