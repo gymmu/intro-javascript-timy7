a6d2d618023260fa64acbc58a65c921669a9e09e
Yeva Skotar <e270611@edu.sbl.ch>
2023-10-31 10:54:42 +0200

    Aufgabe 8

--- script.js
+++ script.js
@@ -10,6 +10,51 @@ function deleteText (){
   const elemToDeleteText = document.querySelector("#to-delete")
 elemToDeleteText.textContent = "Inhalt gelöscht"
 }
-function incremenCounter(){
-  
+function incrementCounter(){
+  const counterElem = document.querySelector("#counter")
+  let num = parseInt(counterElem.textContent)
+  num = num + 1
+  counterElem.textContent = num
+}
+function decrementCounter() {
+  const counterElem = document.querySelector("#counter")
+  let num = parseInt(counterElem.textContent)
+  num = num - 1
+  counterElem.textContent = num
+}
+
+function resetCounter() {
+  const counterElem = document.querySelector("#counter")
+  let num = parseInt(counterElem.textContent)
+  counterElem.textContent = 0
+}
+
+function writeText() {
+  const InputElem = document.querySelector("#input")
+  const OutputElem = document.querySelector("#output")
+  OutputElem.textContent = OutputElem.textContent +"\n" + InputElem.value 
+}
+
+function randomNumber() {
+  const Number = document.querySelector("#number")
+  Number.value = Math.floor(Math.random() * 100)
+}
+
+function randomColor() {
+
+  const colorList = [
+    "red",
+    "rgb(153, 47, 167)"
+  ]
+
+
+  const Randomcolor = document.querySelector("#color")
+  Randomcolor.value = `rgb(${red}, ${orange}, ${yellow}, ${green}, ${blue}, ${lightblue}, ${purple})`
+const red =
+const orange = Math.floor(Math.random() * 255)
+const yellow= Math.floor(Math.random() * 255)
+const blue = Math.floor(Math.random() * 255)
+const green = Math.floor(Math.random() * 255)
+const lightblue = Math.floor(Math.random() * 255)
+const purple = "rgb(153, 47, 167)"
 }
